72cf9fbc5eed73e3a725371dc3e842dd64054bbf
ZimmerMike <angelmetalhj@gmail.com>
2024-06-10 01:17:07 -0600

    fixing flex design, adding opacity overlay when sidebar is open

--- ui/js/sidebar.js
+++ ui/js/sidebar.js
@@ -1,11 +1,19 @@
 const openButton = document.getElementById('open-nav');
 const closeButton = document.getElementById('close-nav');
 const navContainer = document.querySelector('.nav-container');
+const overlay = document.getElementById('overlay');
 
 openButton.addEventListener('click', () => {
     navContainer.classList.toggle('nav-visible');
+    overlay.classList.toggle('overlay-visible');
 });
 
 closeButton.addEventListener('click', () => {
-    navContainer.classList.toggle('nav-visible');
+    navContainer.classList.remove('nav-visible');
+    overlay.classList.remove('overlay-visible');
+});
+
+overlay.addEventListener('click', () => {
+    navContainer.classList.remove('nav-visible');
+    overlay.classList.remove('overlay-visible');
 });
